c7a1adf92706279f4a6140799b07e23fb65fc5b2
Marco Bosi <marco.bosi4@studio.unibo.it>
2018-06-06 07:45:13 +0200

    filename too short

--- src/grid_fields.F90
+++ src/grid_fields.F90
@@ -37,7 +37,7 @@
  
  !----------------TEST OUTPUT OMP
  character (len=10),allocatable::content(:)!one because it has the execution time of the loop it has always the same number and relies on total time
- character (len=20)::filename
+ character (len=30)::filename
  integer::segnalino=1
  !------------
  
@@ -4566,7 +4566,7 @@
  !print*,k2,k1,(j2-j1),(j2-j1)*(n1p-i1)
  !print*,t2-t1,real(it2-it1)/real(rate),j1,j2
  write(content(segnalino),'(es8.2)') real(it2-it1)/real(rate)
- print*,segnalino
+ !print*,segnalino
  segnalino=segnalino+1
  if(nfield <6)return
  if(ndim==3)then
