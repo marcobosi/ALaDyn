480082256e00adc258cfc7734dfa04d5340a7c78
Marco Bosi <marco.bosi4@studio.unibo.it>
2018-12-09 15:45:50 +0100

    cnaf script to avoid using mpirun.lsf

--- scripts/run/cnaf_scorep_64.sh
+++ scripts/run/cnaf_scorep_64.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
-TOTAL_NUMBER_OF_CORES=64
+NUM_OF_MPI_TASKS=64
+OMP_THREADS_PER_MPI_TASK=1
+TOTAL_NUMBER_OF_CORES=$(($NUM_OF_MPI_TASKS * $OMP_THREADS_PER_MPI_TASK))
 
 EXECUTABLE="./ALaDyn"
 stderr_file=epic.txt
@@ -27,12 +28,13 @@ touch ${stdout_file}
   echo "#BSUB -n ${TOTAL_NUMBER_OF_CORES}"
   echo "module purge"
   echo "module load compilers/gcc-4.9.2"
-  echo "module load compilers/openmpi-1.8.1_gcc-4.9.0_with_cuda6.5"
+  echo "module load compilers/openmpi-1.8.4_gcc-4.9.0_cuda6.5"
   echo "module load boost_1_56_0_gcc4_9_0"
   echo "export SCOREP_ENABLE_PROFILING=true"
   echo "export SCOREP_ENABLE_TRACING=false"
   echo "export SCOREP_EXPERIMENT_DIRECTORY=profile"
-  echo "/usr/share/lsf/9.1/linux2.6-glibc2.3-x86_64/bin/mpirun.lsf env PSM_SHAREDCONTEXTS_MAX=8 ${EXECUTABLE} >> ${stdout_file} 2>> ${stderr_file}"
+  echo "/shared/software/compilers/openmpi-1.8.4_gcc4.9.0_cuda6.5/bin/mpirun -np ${NUM_OF_MPI_TASKS} env PSM_SHAREDCONTEXTS_MAX=8 ${EXECUTABLE} >> ${stdout_file} 2>> ${stderr_file}"
+#  echo "/usr/share/lsf/9.1/linux2.6-glibc2.3-x86_64/bin/mpirun.lsf env PSM_SHAREDCONTEXTS_MAX=8 ${EXECUTABLE} >> ${stdout_file} 2>> ${stderr_file}"
 } > $job
 
 echo "Please submit the job with the following command:"
